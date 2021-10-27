711084b72aaa4c08c00ca48a6b0ebd2974f96002
miemii11108 <93252650+miemii11108@users.noreply.github.com>
2021-10-27 16:15:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-
+introtiltle : mi
+introDesc : thich an cut khummmm
+btnItro : hihiihihihihiihi
+tiltle : m thích ăn cứt nói mẹ đi !?
+  btnYes : drui ngày nào t chả ăn cứt hihi
+  btnNo : chụ ui t khum thich ănnn
+  question : eo oiii m thich ăn cứt á cute z
+  btnreply : gửi mie mii </33
+reply : hok pp
+mess : nghiện cứt hơn nghiện t
+messDesc : xong thì qua ib t còn ngồi đây làm gì điên aaa đồ nghiện cứt =)))))))
+fb link : https://www.facebook.com/mie.mii.01/
